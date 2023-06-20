de0035394b7a284ca41c7650147f98619181f504
semawebdev <isemagul@gmail.com>
2023-06-20 14:49:18 +0200

    add alternative solution

--- June/Week III/validParentheses copy.js
+++ June/Week III/validParentheses copy.js
@@ -20,34 +20,59 @@ Example 3:
 Input: s = "(]"
 Output: false */
 
+// SOLUTION I
 var isValid = function (s) {
-// initialize empty stack
+  // initialize empty stack
   let stack = [];
 
-// initialize an object to map through the opening symbols with the corresponding closing symbols
+  // initialize an object to map through the opening symbols with the corresponding closing symbols
   let object = {
     "(": ")",
     "[": "]",
     "{": "}",
   };
 
-// iterate over eaach character in the string starting from 0
+  // iterate over eaach character in the string starting from 0
   for (let i = 0; i < s.length; i++) {
-
-// for each character `s[i]`, check if it's an opening symbol
+    // for each character `s[i]`, check if it's an opening symbol
     if (Object.keys(object).includes(s[i])) {
-// if it's an opening symbol, push the corresponding closing symbol to the stack
+      // if it's an opening symbol, push the corresponding closing symbol to the stack
       stack.push(object[s[i]]);
-/* if it's not an opening symbol, check two conditions: 
+      /* if it's not an opening symbol, check two conditions: 
    - if the stack is empty, there is no corresponding opening symbol in the string; return false 
    - if the `s[i]` doesn't match the top/last element of the stack, it means the closing symbol doesn't have the corresponsing opening symbol; return false*/
     } else if (stack.length === 0 || s[i] !== stack.pop()) {
       return false;
     }
   }
-// after iterating through all the str, check if the stack is empty, it means all symbols matched; return true.  
+  // after iterating through all the str, check if the stack is empty, it means all symbols matched; return true.
   return stack.length === 0;
 };
 
 /* note: `stack[stack.length-1]` does not modify the stack. it simply retrieves the top element. 
 `stack.pop()`retrieves the top element of the stack and removes it from the stack. */
+
+// SOLUTION II
+var isValid = function (s) {
+  let object = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  let stack = [];
+  for (let ch of s) {
+    // if ch exists as a key in object, it's an opening bracket
+    if (object[ch]) {
+      // push the corresponding closing bracket to the stack
+      stack.push(object[ch]);
+      // ch is a closing bracket and top of stack matches
+    } else if (stack.length > 0 && stack[stack.length - 1] === ch) {
+      stack.pop();
+    } else {
+      // ch is a closing bracket and top of the stack doesn't match
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
